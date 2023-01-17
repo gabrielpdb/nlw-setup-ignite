6746eff5c40d625f1c158a6bcb6d7a14f4e0d546
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-01-17 20:25:41 -0300

    server and web, 1st class done

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import { Habit } from './components/Habit'
+
+function App() {
+  return (
+    <>
+      <Habit completed={2} />
+      <Habit completed={3} />
+    </>
+  )
+}
+
+export default App
